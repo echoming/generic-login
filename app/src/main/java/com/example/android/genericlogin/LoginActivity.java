ab02ec4fab08533511c437fdfd81ff645d7f8bd3
Nishant Patel <nishantuwindsor@gmail.com>
2018-05-02 21:25:49 -0400

    Added copyrights

--- app/src/main/java/com/example/android/genericlogin/LoginActivity.java
+++ app/src/main/java/com/example/android/genericlogin/LoginActivity.java
@@ -1,18 +1,19 @@
 /*
-Copyright 2017 Nishant Patel
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the
+ * License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ *
+ * Created by Nishant Patel in 02/05/18 9:25 PM
+ *
+ * Copyright (c) 2018.
+ * Last modified  10/12/17 5:03 PM
+ */
 
 package com.example.android.genericlogin;
 
